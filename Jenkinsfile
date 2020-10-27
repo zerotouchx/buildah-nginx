03e86b52aa66dff5ab5a807378f4bd27d7af9656
manintheit <manintheit@gmail.com>
2020-10-27 19:40:02 +0000

    Added Jenkins file + buildah-build.sh has been modified

---
+++ Jenkinsfile
@@ -0,0 +1,23 @@
+pipeline {
+    agent none
+    stages {
+        stage('BUILD') {
+            agent{label "podman"}
+            steps {
+                sh 'sudo ./buildah-build.sh'
+            }
+        }
+
+        stage('PUSH') {
+            agent{label "podman"}
+            steps {
+            
+            withVault(configuration: [timeout: 60, vaultCredentialId: '835dadcc-6e30-4a33-8983-7ac3cd065ebe', vaultUrl: 'http://127.0.0.1:8200'], vaultSecrets: [[path: 'secret/homelabio/dev/dockerhubmanintheit', secretValues: [[vaultKey: 'username'], [vaultKey: 'password'], [vaultKey: 'registry_url']]]]) {
+            buildah login --username {$username} --password {$password}
+            buildah push buildah-nginx docker://docker.io/zerotouchx/manintheit/buildah-nginx:latest
+}
+            }
+        }
+    
+    }
+}
--- buildah-build.sh
+++ buildah-build.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 set -x
 set -o errexit
-
 # Create a container
 container=$(buildah from --name "buildah-nginx" alpine)
 mount=$(buildah mount $container)
-
 # Labels are part of the "buildah config" command
 buildah config --label maintainer="manintheit <manintheit@gmail.com>" $container
-
-
-
-
 buildah run $container  apk add --no-cache nginx
 buildah run $container  mkdir /www
 buildah run $container  mkdir -p /run/nginx
@@ -21,20 +15,11 @@ buildah run $container  chown -R www:www /www
 mv $mount/etc/nginx/nginx.conf $mount/etc/nginx/nginx.conf.orig
 cp ./conf/nginx.conf $mount/etc/nginx/nginx.conf
 cp ./www/index.html $mount/www/index.html
-
 buildah config --port 8080  $container
 buildah config --entrypoint 'nginx -g "daemon off;"' $container
-#buildah config --cmd  ["daemon off;"]  $container
-
-
 #Uncomment it if you need a debugging...
 #buildah config --cmd ["/bin/sleep 36000"] $container
-
 #For docker format use it --format docker
 #buildah commit --format docker $container hello:latest
 buildah commit  $container buildah-nginx
 buildah unmount $container
-
-
-
-
